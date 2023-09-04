1455d261e5ef940193dfcbdb5ba798526a59199f
Nicko25 <nicolas25menendez@yahoo.com.ar>
2023-09-04 13:18:57 -0300

    se agrego la animacion de rotacion en screen y smartphone

--- src/components/Smartphone.jsx
+++ src/components/Smartphone.jsx
@@ -1,12 +1,29 @@
-import React, { useRef } from 'react'
+import React, { useState, useEffect } from 'react';
 import { useGLTF } from '@react-three/drei'
 
 export default function Model(props) {
   const { nodes, materials } = useGLTF('/smartphone-transformed.glb')
+  const [rotationY, setRotationY] = useState(3.15);
+  const [direction, setDirection] = useState(1); // 1 para incrementar, -1 para decrementar
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (rotationY >= 3.9) {
+        setDirection(-1); // Cambia la dirección cuando llega a 0.9
+      } else if (rotationY <= 2.3) {
+        setDirection(1); // Cambia la dirección cuando llega a -0.9
+      }
+
+      setRotationY(rotationY + direction * 0.01);
+    }, 10); // Cambia la rotación cada 10 milisegundos
+
+    return () => clearInterval(interval); // Limpia el intervalo cuando el componente se desmonta
+  }, [rotationY, direction]);
+  
   return (
-    <group {...props} dispose={null}>
+    <group {...props} dispose={null} rotation={[Math.PI, rotationY, Math.PI]}>
       <group position={[0.1, 2.4, 0]}>
-        <mesh geometry={nodes.Object_12.geometry} material={materials.Color_1} />
+        <mesh geometry={nodes.Object_12.geometry} material={materials.Color_1}/>
         <mesh geometry={nodes.Object_13.geometry} material={materials.Black} />
       </group>
       <group position={[0.1, 2.4, 0]}>
